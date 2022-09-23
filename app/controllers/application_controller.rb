d9513b2ae8aa69d1128479b1891600feed68b611
Furqan Haroon <furqan@isoftstudios.com>
2022-09-23 17:24:57 +0500

    Update user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lname, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :lname, :avatar)}
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :lname, :avatar, :email, :password, :current_password)}
   end
 end
