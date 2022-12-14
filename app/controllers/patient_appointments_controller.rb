3e2e3dd21d5dc9a0657645c0b43257ff56e09e62
Furqan Haroon <furqan@isoftstudios.com>
2022-12-14 18:02:29 +0500

    add notification

--- app/controllers/patient_appointments_controller.rb
+++ app/controllers/patient_appointments_controller.rb
@@ -5,19 +5,18 @@ class PatientAppointmentsController < ApplicationController
 
   def index
     @patient_appointments = PatientAppointment.all if current_user.admin? || current_user.doctor?
-    return unless current_user.patient?
-
-    @patient_appointments = current_user.patient_appointments.all
-
     if params[:appointment_date].blank?
-      @patient_appointments = PatientAppointment.not_yet_done.order("created_at DESC")
+      if current_user.patient?
+        @patient_appointments = current_user.patient_appointments.not_yet_done.order("created_at DESC")
+      end
     end 
-
   end
 
   def history
     if params[:appointment_date].blank?
-      @patient_appointments = PatientAppointment.previous_appointment.order("created_at DESC")
+      if current_user.patient?
+        @patient_appointments = current_user.patient_appointments.previous_appointment.order("created_at DESC")
+      end
     end   
   end  
 
@@ -33,15 +32,17 @@ class PatientAppointmentsController < ApplicationController
       s = params[:date].delete! '""'
       date = Date.parse(s)
       @patient_appointment = current_user.patient_appointments.create(user_id: params[:user], date: date)
+      @notification = Notification.create(patient_appointment_id: @patient_appointment.id, doctor_id: params[:user], user_id: current_user.id, notifiable_id: @patient_appointment.id, notifiable_type: PatientAppointment)
+      @notification.save
       user = current_user
       @user = user.email
       PatientAppointmentMailer.appointment_created(@user).deliver_now
     end
-    respond_to do |_format|
+    respond_to do |format|
       SendEmailToPatientJob.perform_later
       redirect_to patient_appointments_url
-      flash[:notice] = 'Appointment was successfully created.'
-    end
+      flash[:notice] = 'Appointment was successfully created.' 
+    end   
   end
 
   def show
@@ -49,8 +50,8 @@ class PatientAppointmentsController < ApplicationController
   end
 
   def destroy
-    @patient_appointment = PatientAppointment.find(params[:id])
     authorize @patient_appointment
+    @patient_appointment = PatientAppointment.find(params[:id])
     @patient_appointment.destroy
     respond_to do |format|
       format.html { redirect_to patient_appointments_url, notice: 'Selected appointment was successfully destroyed.' }
@@ -68,7 +69,7 @@ class PatientAppointmentsController < ApplicationController
   end
 
   def patient_appointment_params
-    params.require(:patient_appointment).permit(:user_id, :date)
+    params.require(:patient_appointment).permit(:user_id, :date, :notification_ids)
   end
 
   def current_appointment
