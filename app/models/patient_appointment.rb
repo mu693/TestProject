08101cf0b0d9fb25b4f372458a0898f38af32183
Furqan Haroon <furqan@isoftstudios.com>
2022-12-14 18:04:23 +0500

    add notification

--- app/models/patient_appointment.rb
+++ app/models/patient_appointment.rb
@@ -1,8 +1,6 @@
 class PatientAppointment < ApplicationRecord
   belongs_to :user
-
+  has_many :notifications, :as => :notifiable, dependent: :destroy 
   scope :not_yet_done, -> { where('date > ?', Time.now) } 
-
-  scope :previous_appointment, -> { where('date < ?', Time.now) } 
-
+  scope :previous_appointment, -> { where('date < ?', Time.now) }
 end
